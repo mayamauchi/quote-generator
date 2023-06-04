1bc8a8c8598100526ccc40664dd5d04db425651a
Maaya <yamauchimaaya@gmail.com>
2023-06-03 21:31:31 -0500

    figuring out cssoveride

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,58 @@
-import {useState, useEffect} from 'react';
-
-import Quote from './components/Quote'
+import { useState, useEffect } from "react";
+import Quote from "./components/Quote";
+import PuffLoader from "react-spinners/PuffLoader";
 
 function App() {
   const [quote, setQuote] = useState({
     author: null,
-    description: null
-  })
+    description: null,
+  });
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchQuote = async () => {
-    // return await fetch("https://animechan.vercel.app/api/random")
-    return await fetch("http://localhost:8080/api/quotes")
-
-    .then((response) => response.json());
-    
-
-  }
+    return await fetch("http://localhost:8080/api/quotes/random").then(
+      (response) => response.json()
+    );
+  };
 
   const generate = async () => {
     setQuote(await fetchQuote());
-  }
+    setIsLoading(true);
 
-  useEffect(() => { fetchQuote().then(quote => setQuote(quote)); }, []);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchQuote().then((quote) => setQuote(quote));
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1800);
+  }, []);
+
 
   return (
-<div className="App">
-    <Quote quote={quote}/>
-    <button onClick={generate}>Generate New Quote</button>
-</div>
-  )
+    <div className="App">
+      {isLoading ? (
+        <div id="loader">
+          <div id="loader-container">
+            <PuffLoader
+              id="puffloader"
+              size={500}
+              color={"#04724D"}
+              isLoading={isLoading}
+              // css={overide}
+              //figure out how to do cssoveride. Possibly last year's version? 
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="quoteblock">
+          <Quote quote={quote} />
+          <button onClick={generate}>Generate New Quote</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Quote.jsx
+++ frontend/src/components/Quote.jsx
@@ -1,20 +1,16 @@
-import React from 'react';
 
 const Quote = ({ quote }) => {
-  console.log(quote);
-
   
   return (
     <div className="quote">
-      <div className="anime" title={quote.anime}>
-        {quote.anime}
-      </div>
       <blockquote>{quote.description}</blockquote>
-      <div className="character" title={quote.author}>
+      <div className="author" title={quote.author}>
         {quote.author}
       </div>
     </div>
   );
 };
+
+
 
 export default Quote;
