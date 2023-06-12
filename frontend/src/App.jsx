eac7d209f768c8011e05812b46e38f8452fae016
Maaya <yamauchimaaya@gmail.com>
2023-06-11 22:45:25 -0500

    added more functions to make quote generating random and have text with loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,55 @@ import PuffLoader from "react-spinners/PuffLoader";
 
 function App() {
   const [quote, setQuote] = useState({
+    id: null,
     author: null,
     description: null,
   });
   const [isLoading, setIsLoading] = useState(true);
+  const [quoteCount, setQuoteCount] = useState(0);
+  const [quoteIdQueue, setQuoteIdQueue] = useState([]);
 
   const fetchQuote = async () => {
-    return await fetch("http://localhost:8080/api/quotes/random").then(
-      (response) => response.json()
+    if (quoteIdQueue.length === 0) {
+      generateQuoteIdQueue(quoteCount);
+    }
+    console.log(quoteIdQueue)
+    let id = quoteIdQueue.pop();
+    const response = await (
+      await fetch(`http://localhost:8080/api/quotes/${id}`)
+    ).json();
+    return response;
+  };
+
+  const generateQuoteIdQueue = (count) => {
+    setQuoteIdQueue(
+      Array
+        .from({length: count}, (_, i) => i + 1)
+        .sort(() => Math.random() - 0.5)
     );
   };
 
-  const generate = async () => {
+  const generate = async () => { 
     setQuote(await fetchQuote());
     setIsLoading(true);
-
     setIsLoading(false);
   };
 
   useEffect(() => {
+    fetch(`http://localhost:8080/api/quotes/count`).then(async (res) => {
+      let response = await res.json();
+      setQuoteCount(response.count);
+      generateQuoteIdQueue(response.count);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (quoteCount === 0 || quoteIdQueue.length === 0) return;
     fetchQuote().then((quote) => setQuote(quote));
     setTimeout(() => {
       setIsLoading(false);
     }, 1800);
-  }, []);
-
+  }, [quoteCount, quoteIdQueue])
 
   return (
     <div className="App">
@@ -39,10 +63,10 @@ function App() {
               id="puffloader"
               size={500}
               color={"#04724D"}
-              isLoading={isLoading}
-              css={overide}
-              //figure out how to do cssoveride. Possibly last year's version? 
             />
+            <div className="loader-text">
+              <p>Hey you!</p>
+            </div>
           </div>
         </div>
       ) : (
