8b5151e6dac77b5d237ae7f920f53812fde734c9
Maaya <yamauchimaaya@gmail.com>
2023-06-14 18:47:02 -0500

    heroku deploy

---
+++ db/quotedata.js
@@ -0,0 +1,133 @@
+const { client } = require("./client");
+
+const quoteData = [
+    {
+        id: 1,
+        author: "Audrey Hepburn",
+        description: "Nothing is impossible. The word itself reads \"I'm possible\"."
+    },
+    {
+        id: 2,
+        author: "Unknown",
+        description: "You can't pour from an empty cup."
+    },
+    {
+        id: 3,
+        author: "Risa Oribe",
+        description: "The most beautiful flower is the one that bloomed from continuous effort, even in a thorny path."
+    },
+    {
+        id: 4,
+        author: "Software Developers",
+        description: "We, as software developers, are paid to be frustrated."
+    },
+    {
+        id: 5,
+        author: "Theodore Roosevelt",
+        description: "Comparison is the thief of joy."
+    },
+    {
+        id: 6,
+        author: "Stephen McCranie",
+        description: "The master has failed more times than the beginner has even tried."
+    },
+    {
+        id: 7,
+        author: "Confucius",
+        description: "A man is great not because he hasn't failed; a man is great because failure hasn't stopped him."
+    },
+    {
+        id: 8,
+        author: "Albert Einstein",
+        description: "Never study to be successful, study for self efficiency. Don't run behind success. Follow behind excellence, success will come all way behind you."
+    },
+    {
+        id: 9,
+        author: "Swami Vivekananda",
+        description: "All power is within you; you can do anything and everything. Believe in that, do not believe that you are weak; do not believe that you are half-crazy lunatics, as most of us do nowadays. You can do any thing and everything, without even the guidance of any one. Stand up and express the divinity within you."
+    },
+    {
+        id: 10,
+        author: "Unknown",
+        description: "Only compare yourself to the past version of yourself."
+    },
+    {
+        id: 11,
+        author: "Unknown",
+        description: "Fake it 'til you make it."
+    },
+    {
+        id: 12,
+        author: "Unknown",
+        description: "Practice makes progress."
+    },
+    {
+        id: 13,
+        author: "Unknown",
+        description: "The solution exists, I just haven't found it yet."
+    },
+    {
+        id: 14,
+        author: "Unknown",
+        description: "Defeat insecurity/anxiety with preparation and hard work."
+    },
+    {
+        id: 15,
+        author: "Unknown",
+        description: "There is no easy way from the earth to the stars."
+    },
+    {
+        id: 16,
+        author: "Unknown",
+        description: "Success is the capacity to take what was only once imagined and make it real."
+    },
+    {
+        id: 17,
+        author: "Alan W. Watts",
+        description: "The only way to make sense out of change is to plunge into it, move with it, and join the dance."
+    },
+    {
+        id: 18,
+        author: "Unknown",
+        description: "Sometimes the best thing you can do is not think, not wonder, not imagine, not obsess. Just breathe, and have faith that everything will work out for the best."
+    },
+    {
+        id: 19,
+        author: "Unknown",
+        description: "Part of progress is regress."
+    },
+    {
+        id: 20,
+        author: "Unknown",
+        description: "Too strong to fail or too weak to try?"
+    },
+    {
+        id: 21,
+        author: "Louise L. Hay",
+        description: "Remember, you have been criticizing yourself for years and it hasn't worked. Try approving of yourself and see what happens."
+    },
+    {
+        id: 22,
+        author: "Unknown",
+        description: "You are not an imposter; you are a work in progress."
+    },
+    {
+        id: 23,
+        author: "Unknown",
+        description: "Success is not about being perfect; it's about making progress and embracing your journey."
+    },
+    {
+        id: 24,
+        author: "Christian D. Larson",
+        description: "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle."
+    },
+    {
+        id: 25,
+        author: "Unknown",
+        description: "Embrace your accomplishments, for they are a testament to your abilities and hard work."
+    }
+
+]
+module.exports = {
+quoteData
+};
