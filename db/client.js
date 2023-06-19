f6db7529b0b8cf30abce562f669ec33a50cb875e
Maaya <yamauchimaaya@gmail.com>
2023-06-18 19:24:09 -0500

    added database url for heroku

--- db/client.js
+++ db/client.js
@@ -2,7 +2,7 @@ const { Client } = require("pg");
 
 const client = new Client({
   connectionString:
-    process.env.DATABASE_URL || "postgres://localhost:5432/quote_generator",
+    process.env.DATABASE_URL || "postgres://sdikefiyptbllv:1baf9e5de4c2a3c6d2746df83589450be8670ed23face91a3e0db4313966b056@ec2-52-0-187-246.compute-1.amazonaws.com:5432/daejmd4nvrp4rt",
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
